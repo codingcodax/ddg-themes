19d976a833ceb339493c760e877a1e84e5134630
Alexis Guzman <codingcodax@gmail.com>
2022-12-31 14:14:07 -0600

    feat(header): use logo dynamic for dark and light themes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,20 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePrefersColorScheme } from '@anatoliygatt/use-prefers-color-scheme';
 
 const Header = () => {
+  const prefersColorScheme = usePrefersColorScheme({ ssr: true });
+  const isDarkMode = prefersColorScheme === 'dark';
+
+  console.log(prefersColorScheme);
+
   return (
     <header className='py-2 px-4'>
       <Link href='/'>
         <Image
           alt='duck duck go themes logo'
           height={32}
-          src='/images/logo.svg'
+          src={`/images/${isDarkMode ? 'logo-dark' : 'logo-light'}.svg`}
           width={158}
         />
       </Link>
