4f394e3a323c4ffc0c56cee638088f47f2b5e735
Alexis Guzman <codingcodax@gmail.com>
2022-12-31 16:14:52 -0600

    chore(tailwind): add some ddg color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      white: 'white',
+      base: 'rgb(var(--gray-90) / <alpha-value>)',
+      'gray-90': '#222',
+      'blue-30': '#7295f6',
+      'blue-40': '#557ff3',
+      'blue-50': '#3969ef',
+      'blue-60': '#2b55ca',
+    },
     extend: {},
   },
   plugins: [],
