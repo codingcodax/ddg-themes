50dd127cb00d78df0f3a97317c2f43482e523329
Alexis Guzman <codingcodax@gmail.com>
2022-12-31 11:48:47 -0600

    feat(app): use metropolis font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,38 @@
 import type { AppProps } from 'next/app';
+import localFont from '@next/font/local';
 
 import Layout from '~/components/layout';
 
 import '../styles/globals.css';
 
+const metropolis = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Metropolis-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Metropolis-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <style global jsx>{`
+        html {
+          font-family: ${metropolis.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
