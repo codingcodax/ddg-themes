0615cc85e6dc4ea137bcab24c552f0dd09110754
Alexis Guzman <codingcodax@gmail.com>
2022-12-31 12:06:02 -0600

    feat(layout): setup header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Header = () => {
   return (
-    <div>
-      <p>Header component</p>
-    </div>
+    <header className='py-2 px-4'>
+      <Link href='/'>
+        <Image
+          alt='duck duck go themes logo'
+          height={32}
+          src='/images/logo.svg'
+          width={158}
+        />
+      </Link>
+    </header>
   );
 };
 
