e77fa797106ebd38ba54f295d3c9021bcdbb6c96
Alexis Guzman <codingcodax@gmail.com>
2023-01-02 17:06:57 -0600

    feat(themes): add nord

--- src/data/themes/index.ts
+++ src/data/themes/index.ts
@@ -3,8 +3,9 @@ import dracula from './dracula';
 import hack from './hack';
 import navyTurquoise from './navyTurquoise';
 import neon from './neon';
+import nord from './nord';
 import titanium from './titanium';
 
-const Themes = [cyberpunk, dracula, hack, navyTurquoise, neon, titanium];
+const Themes = [cyberpunk, dracula, hack, navyTurquoise, neon, nord, titanium];
 
 export default Themes;
---
+++ src/data/themes/nord.ts
@@ -0,0 +1,12 @@
+import type { Theme } from '~/types';
+
+const nord: Theme = {
+  name: 'Nord',
+  screenshot: '',
+  cookie:
+    // '7=101116; j=070709; 9=00ff2b; aa=0cbd2b; 8=d1d1d1; x=fffc58; 21=118b25; ',
+    '7=2e3440; 9=81a1c1; 8=81a1c1; aa=87c0d0; x=b28ead; 21=81a1c1; j=404855;',
+  palette: ['#2e3440', '#404855', '#81a1c1', '#87c0d0', '#b28ead'],
+};
+
+export default nord;
