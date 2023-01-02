bc80dd8bfef4ea77c083b725f829cdd1f3acbcbc
Alexis Guzman <codingcodax@gmail.com>
2023-01-02 13:02:56 -0600

    feat(footer): add responsive paddings

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='mt-16 px-4 pb-2'>
+    <footer className='mt-16 px-4 pb-2 md:px-10 md:pt-8 lg:px-16'>
       <div className='h-px w-full bg-gray-90/20 dark:bg-white/20' />
       <p className='mt-8'>
         Crafted by{' '}
