effa8f9924169f103cc3a9d385d619d98cba72a4
Alexis Guzman <codingcodax@gmail.com>
2023-01-01 20:24:16 -0600

    feat(themes): add navy turquoise

---
+++ src/data/themes/navyTurquoise.ts
@@ -0,0 +1,10 @@
+import type { Theme } from '~/types';
+
+const navyTurquoise: Theme = {
+  name: 'Navy Turquoise',
+  screenshot: '',
+  cookie:
+    '7=0b1021; j=080813; 9=00af87; aa=0a7355; 8=d3d5e5; x=a8d3ff; 21=080813;',
+};
+
+export default navyTurquoise;
