657920f62edca19ebf3c211c748b2c886dcc4207
Alexis Guzman <codingcodax@gmail.com>
2023-01-02 14:23:53 -0600

    feat(themes): add hack

---
+++ src/data/themes/hack.ts
@@ -0,0 +1,19 @@
+import type { Theme } from '~/types';
+
+const hack: Theme = {
+  name: 'Hack',
+  screenshot: '',
+  cookie:
+    '7=101116; j=070709; 9=00ff2b; aa=0cbd2b; 8=d1d1d1; x=fffc58; 21=118b25; ',
+  palette: [
+    '#101116',
+    '#070709',
+    '#00ff2b',
+    '#0cbd2b',
+    '#d1d1d1',
+    '#fffc58',
+    '#118b25',
+  ],
+};
+
+export default hack;
--- src/data/themes/index.ts
+++ src/data/themes/index.ts
@@ -1,8 +1,9 @@
 import cyberpunk from './cyberpunk';
 import dracula from './dracula';
+import hack from './hack';
 import navyTurquoise from './navyTurquoise';
 import titanium from './titanium';
 
-const Themes = [cyberpunk, dracula, navyTurquoise, titanium];
+const Themes = [cyberpunk, dracula, hack, navyTurquoise, titanium];
 
 export default Themes;
