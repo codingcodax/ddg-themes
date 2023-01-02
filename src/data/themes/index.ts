7b053dcd793624cb1e069b5d5cc14f9f33291386
Alexis Guzman <codingcodax@gmail.com>
2023-01-02 14:29:23 -0600

    feat(themes): add neon

--- src/data/themes/index.ts
+++ src/data/themes/index.ts
@@ -2,8 +2,9 @@ import cyberpunk from './cyberpunk';
 import dracula from './dracula';
 import hack from './hack';
 import navyTurquoise from './navyTurquoise';
+import neon from './neon';
 import titanium from './titanium';
 
-const Themes = [cyberpunk, dracula, hack, navyTurquoise, titanium];
+const Themes = [cyberpunk, dracula, hack, navyTurquoise, neon, titanium];
 
 export default Themes;
