86a902c0e78d1bf87f50a47c992341e03123ee87
Alexis Guzman <codingcodax@gmail.com>
2023-01-03 12:28:56 -0600

    feat(utils): create apply theme function string

---
+++ src/utils/applyTheme.ts
@@ -0,0 +1,32 @@
+const applyTheme = `// Cookies string for your theme
+const cookie = 'COOKIE';
+
+// Converts cookie string into formatted JSON
+const cookieToJSON = (cookieRaw) => {
+  const cookieJson = {};
+  const items = cookieRaw.split(/[ ,]+/);
+
+  items.forEach((item) => {
+    const parts = item.split('=');
+    cookieJson[parts[0]] = parts[1];
+  });
+
+  return cookieJson;
+};
+
+// Iterates over JSON, and adds to browser cookie store
+const setCookies = (cookieJson) => {
+  Object.keys(cookieJson).forEach((key) => {
+    document.cookie = \`$\{key}=$\{cookieJson[key]}\`;
+  });
+};
+
+alert('The NAME theme will be applied, are you sure?');
+
+// Call set cookies, passing in formated cookie data
+setCookies(cookieToJSON(cookie));
+
+// All done, reload page for changes to take effect :)
+location.reload();`;
+
+export default applyTheme;
