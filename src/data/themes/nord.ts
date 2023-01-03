c90783cc01fc32f91e96dd09229d24bdd8a4f07f
Alexis Guzman <codingcodax@gmail.com>
2023-01-03 16:53:13 -0600

    feat(themes): update nord colors

--- src/data/themes/nord.ts
+++ src/data/themes/nord.ts
@@ -4,9 +4,8 @@ const nord: Theme = {
   name: 'Nord',
   screenshot: '',
   cookie:
-    // '7=101116; j=070709; 9=00ff2b; aa=0cbd2b; 8=d1d1d1; x=fffc58; 21=118b25; ',
-    '7=2e3440; 9=81a1c1; 8=81a1c1; aa=87c0d0; x=b28ead; 21=81a1c1; j=404855;',
-  palette: ['#2e3440', '#404855', '#81a1c1', '#87c0d0', '#b28ead'],
+    '7=2e3440; j=2e3440; 9=88c0d0; 8=8fbcbb; aa=d8dee9; x=b48ead; 21=3b4252;',
+  palette: ['#2e3440', '#88c0d0', '#8fbcbb', '#d8dee9', '#b48ead', '#3b4252'],
 };
 
 export default nord;
