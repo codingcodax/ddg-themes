752cd4ee70d25cf9aa48b48bf026668d93d78785
Alexis Guzman <codingcodax@gmail.com>
2023-01-03 16:03:55 -0600

    feat(themes): update gruvbox light colors

--- src/data/themes/gruvbox-light.ts
+++ src/data/themes/gruvbox-light.ts
@@ -3,8 +3,9 @@ import type { Theme } from '~/types';
 const gruvboxLight: Theme = {
   name: 'Gruvbox Light',
   screenshot: '',
-  cookie: 'j=83a598; 9=fe8019; aa=282828; 8=fbf1c7; x=d79921; 21=32302f;',
-  palette: ['#83a598', '#fe8019', '#282828', '#fbf1c7', '#d79921', '#32302f'],
+  cookie:
+    '7=fbf1c7, j=fbf1c7; 9=d65d0e; aa=d79921; 8=282828; x=98971a; 21=f2e5bc;',
+  palette: ['#fbf1c7', '#d65d0e', '#d79921', '#282828', '#98971a', '#f2e5bc'],
 };
 
 export default gruvboxLight;
