63b56299bdd078713708a596bca81dffbb49c029
Alexis Guzman <codingcodax@gmail.com>
2023-01-03 16:25:04 -0600

    feat(themes): update navy turquoise colors

--- src/data/themes/navyTurquoise.ts
+++ src/data/themes/navyTurquoise.ts
@@ -4,8 +4,8 @@ const navyTurquoise: Theme = {
   name: 'Navy Turquoise',
   screenshot: '',
   cookie:
-    '7=0b1021; j=080813; 9=00af87; aa=0a7355; 8=d3d5e5; x=a8d3ff; 21=080813;',
-  palette: ['#0b1021', '#080813', '#00af87', '#0a7355', '#d3d5e5', '#a8d3ff'],
+    '7=0b1021; j=0b1021; 9=00af87; aa=0a7355; 8=d3d5e5; x=a8d3ff; 21=080813;',
+  palette: ['#0b1021', '#00af87', '#0a7355', '#d3d5e5', '#a8d3ff', '#080813'],
 };
 
 export default navyTurquoise;
