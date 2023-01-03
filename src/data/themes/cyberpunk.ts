d5f4c9b0ed17b1ca23bf1661359b9b8cc40e3368
Alexis Guzman <codingcodax@gmail.com>
2023-01-03 12:43:35 -0600

    feat(themes): update snippet color for cyberpunk

--- src/data/themes/cyberpunk.ts
+++ src/data/themes/cyberpunk.ts
@@ -4,8 +4,8 @@ const cyberpunk: Theme = {
   name: 'Cyberpunk',
   screenshot: '',
   cookie:
-    '7=261d45; j=372963; 9=0084ff; aa=3d5afe; 8=bbbbbb; x=00ff9c; 21=372963;',
-  palette: ['#261d45', '#372963', '#0084ff', '#3d5afe', '#bbbbbb', '#00ff9c'],
+    '7=261d45; j=372963; 9=0084ff; aa=3d5afe; 8=eeffff; x=00ff9c; 21=372963;',
+  palette: ['#261d45', '#372963', '#0084ff', '#3d5afe', '#eeffff', '#00ff9c'],
 };
 
 export default cyberpunk;
