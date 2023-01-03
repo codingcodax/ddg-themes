c56b5daf7e1bbaebd8e76e960765de606ccc67eb
Alexis Guzman <codingcodax@gmail.com>
2023-01-03 13:19:04 -0600

    feat(themes): add highlight color to dracula

--- src/data/themes/dracula.ts
+++ src/data/themes/dracula.ts
@@ -3,8 +3,9 @@ import type { Theme } from '~/types';
 const dracula: Theme = {
   name: 'Dracula',
   screenshot: '',
-  cookie: '7=282a36; j=282a36; 9=50fa7b; aa=bd93f9; 8=f8f8f2; x=f1fa8c;',
-  palette: ['#282a36', '#50fa7b', '#bd93f9', '#f8f8f2', '#f1fa8c'],
+  cookie:
+    '7=282a36; j=282a36; 9=50fa7b; aa=bd93f9; 8=f8f8f2; x=f1fa8c; 21=44475a;',
+  palette: ['#282a36', '#50fa7b', '#bd93f9', '#f8f8f2', '#f1fa8c', '#44475a'],
 };
 
 export default dracula;
