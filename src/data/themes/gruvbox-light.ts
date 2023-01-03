c445b220ba0e6109daea044e033c514806123004
Alexis Guzman <codingcodax@gmail.com>
2023-01-03 17:51:27 -0600

    fix(themes): update cookie strings

--- src/data/themes/gruvbox-light.ts
+++ src/data/themes/gruvbox-light.ts
@@ -4,7 +4,7 @@ const gruvboxLight: Theme = {
   name: 'Gruvbox Light',
   screenshot: '',
   cookie:
-    '7=fbf1c7, j=fbf1c7; 9=d65d0e; aa=d79921; 8=282828; x=98971a; 21=f2e5bc;',
+    '7=fbf1c7; j=fbf1c7; 9=d65d0e; aa=d79921; 8=282828; x=98971a; 21=f2e5bc;',
   palette: ['#fbf1c7', '#d65d0e', '#d79921', '#282828', '#98971a', '#f2e5bc'],
 };
 
