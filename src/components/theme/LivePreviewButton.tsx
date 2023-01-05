90c128b07d3d317c78116a0b9f4b5b296ff0df21
Alexis Guzman <codingcodax@gmail.com>
2023-01-04 20:57:01 -0600

    feat(theme): setup live preview button

--- src/components/theme/LivePreviewButton.tsx
+++ src/components/theme/LivePreviewButton.tsx
@@ -1,8 +1,23 @@
-const LivePreviewButton = () => {
+import type { Theme } from '~/types';
+import { Icons } from '~/components';
+
+interface LivePreviewButtonProps {
+  cookie: Theme['cookie'];
+}
+
+const LivePreviewButton = ({ cookie }: LivePreviewButtonProps) => {
   return (
-    <div>
-      <p>Live Preview Button</p>
-    </div>
+    <a
+      className='ghost-button flex items-center text-sm'
+      href={`https://duckduckgo.com/?k${cookie
+        .slice(0, -1)
+        .split('; ')
+        .join('&k')}`}
+      rel='noopener noreferrer'
+      target='_blank'
+    >
+      Live Preview <Icons.OpenNewWindow className='ml-2 h-4 w-4' />
+    </a>
   );
 };
 
