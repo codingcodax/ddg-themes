b752ba0f0710378651021cf15b3dcacb6b47d214
Alexis Guzman <codingcodax@gmail.com>
2023-01-04 21:01:41 -0600

    feat(theme): setup get theme button

--- src/components/theme/GetThemeButton.tsx
+++ src/components/theme/GetThemeButton.tsx
@@ -1,8 +1,45 @@
-const GetThemeButton = () => {
+import { useState } from 'react';
+
+import type { Theme } from '~/types';
+import { applyTheme, copyToClipboard } from '~/utils';
+import { Icons } from '~/components';
+
+interface GetThemeButtonProps {
+  name: Theme['name'];
+  cookie: Theme['cookie'];
+}
+
+const GetThemeButton = ({ name, cookie }: GetThemeButtonProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const handleCopy = () => {
+    copyToClipboard(applyTheme.replace('COOKIE', cookie).replace('NAME', name));
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
   return (
-    <div>
-      <p>Get Theme Button</p>
-    </div>
+    <button
+      className={`ghost-button flex items-center text-sm ${
+        isCopied
+          ? 'cursor-not-allowed text-green-50 hover:bg-green-60/20 hover:text-green-60 dark:text-green-30 dark:hover:bg-green-40/20 dark:hover:text-green-40'
+          : ''
+      }`}
+      disabled={isCopied}
+      type='button'
+      onClick={handleCopy}
+    >
+      {isCopied ? (
+        <>
+          Copied <Icons.Check className='ml-2 h-4 w-4' />
+        </>
+      ) : (
+        <>
+          Get Theme <Icons.Copy className='ml-2 h-4 w-4' />
+        </>
+      )}
+    </button>
   );
 };
 
