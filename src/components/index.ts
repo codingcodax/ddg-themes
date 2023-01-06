f65e66b6d3f9d6c54fe3500afc3b74465607f44b
Alexis Guzman <codingcodax@gmail.com>
2023-01-06 17:48:04 -0600

    fix(components): update themes export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Icons } from './icons';
 export { default as Hero } from './Hero';
 export { default as Seo } from './Seo';
-export { default as Themes } from './Themes';
+export { default as Themes } from './themes';
