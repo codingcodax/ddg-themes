4c8a6534cb33f76bb49314e286702465f15bc8b0
Alexis Guzman <codingcodax@gmail.com>
2023-01-05 20:40:26 -0600

    feat(icons): add nav arrow right

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,7 +1,8 @@
 import Check from './Check';
 import Copy from './Copy';
+import NavArrowRight from './NavArrowRight';
 import OpenNewWindow from './OpenNewWindow';
 
-const Icons = { Check, Copy, OpenNewWindow };
+const Icons = { Check, Copy, NavArrowRight, OpenNewWindow };
 
 export default Icons;
