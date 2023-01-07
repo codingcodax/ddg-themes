9db3e20c1cf464e95646d91f01ecf626420bb31f
Alexis Guzman <codingcodax@gmail.com>
2023-01-06 21:01:47 -0600

    feat(themes): setup searchbar

--- src/components/themes/Searchbar.tsx
+++ src/components/themes/Searchbar.tsx
@@ -1,8 +1,17 @@
-const Searchbar = () => {
+interface SearchbarProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Searchbar = ({ value, onChange }: SearchbarProps) => {
   return (
-    <div>
-      <p>Searchbar</p>
-    </div>
+    <input
+      className='h-11 w-full rounded-lg bg-black/5 px-5 focus:bg-transparent dark:bg-white/10'
+      placeholder='Enter the theme name'
+      type='text'
+      value={value}
+      onChange={onChange}
+    />
   );
 };
 
