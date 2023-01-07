90c8515bb932a0f21b92a0ae238df5e61eaca5f7
Alexis Guzman <codingcodax@gmail.com>
2023-01-06 21:02:16 -0600

    feat(themes): add searchbar and select components

--- src/components/themes/index.tsx
+++ src/components/themes/index.tsx
@@ -1,16 +1,43 @@
+import { useMemo, useState } from 'react';
+
 import ThemesData from '~/data/themes';
 
+import Searchbar from './Searchbar';
+import Select from './Select';
 import Theme from './theme';
 
 const Themes = () => {
+  const [typeSelected, setTypeSelected] = useState('all');
+  const [search, setSearch] = useState('');
+
+  const filteredThemes = useMemo(() => {
+    const themesByType =
+      typeSelected === 'all'
+        ? ThemesData
+        : ThemesData.filter((theme) => theme.type === typeSelected);
+
+    return themesByType.filter((theme) =>
+      theme.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [search, typeSelected]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <section className='mx-auto mt-16 w-full max-w-screen-lg px-4 md:px-8 lg:px-16'>
-      <h2 className='mb-10 text-center md:mb-8 md:text-left' id='themes'>
+      <h2 className='mb-4 text-center md:text-left' id='themes'>
         Themes
       </h2>
 
+      <div className='mb-10 flex space-x-2 px-4 md:w-1/2 md:px-0'>
+        <Searchbar value={search} onChange={handleSearch} />
+        <Select value={typeSelected} onValueChange={setTypeSelected} />
+      </div>
+
       <ul className='grid gap-8 px-4 sm:grid-cols-2 md:px-0 lg:grid-cols-3'>
-        {ThemesData.map((theme) => (
+        {filteredThemes.map((theme) => (
           <Theme key={theme.name} {...theme} />
         ))}
       </ul>
