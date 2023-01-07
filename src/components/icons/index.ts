05af9ca9708507ffd8c3fdafd5d58d62ec8a6b04
Alexis Guzman <codingcodax@gmail.com>
2023-01-06 20:31:09 -0600

    feat(icons): add nav arrow up

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,6 +3,7 @@ import Copy from './Copy';
 import NavArrowDown from './NavArrowDown';
 import NavArrowLeft from './NavArrowLeft';
 import NavArrowRight from './NavArrowRight';
+import NavArrowUp from './NavArrowUp';
 import OpenNewWindow from './OpenNewWindow';
 
 const Icons = {
@@ -11,6 +12,7 @@ const Icons = {
   NavArrowDown,
   NavArrowLeft,
   NavArrowRight,
+  NavArrowUp,
   OpenNewWindow,
 };
 
