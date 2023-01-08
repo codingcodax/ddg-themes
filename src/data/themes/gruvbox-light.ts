68859a1721b3c5b5ecddcacc36244e5ed9b69681
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:37:08 -0600

    fix(themes): update gruvbox light palette

--- src/data/themes/gruvbox-light.ts
+++ src/data/themes/gruvbox-light.ts
@@ -4,7 +4,14 @@ const gruvboxLight: Theme = {
   name: 'Gruvbox Light',
   cookie:
     '7=fbf1c7; j=fbf1c7; 9=d65d0e; aa=d79921; 8=282828; x=98971a; 21=f2e5bc;',
-  palette: ['#fbf1c7', '#d65d0e', '#d79921', '#282828', '#98971a', '#f2e5bc'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#fbf1c7' },
+    { name: 'Title', hexCode: '#d65d0e' },
+    { name: 'Visited Title', hexCode: '#d79921' },
+    { name: 'Snippet', hexCode: '#282828' },
+    { name: 'URL', hexCode: '#98971a' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#f2e5bc' },
+  ],
   type: 'light',
 };
 
