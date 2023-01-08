b5febb37f1ee55bd852a720a28866e226ebf2af3
Alexis Guzman <codingcodax@gmail.com>
2023-01-07 19:31:55 -0600

    fix(themes): update ayu light palette

--- src/data/themes/ayu-light.ts
+++ src/data/themes/ayu-light.ts
@@ -4,7 +4,14 @@ const ayuLight: Theme = {
   name: 'Ayu Light',
   cookie:
     '7=f8f9fa; j=f8f9fa; 9=fa8d3e; aa=f2ae49; 8=5c6166; x=399ee6; 21=e7eaed;',
-  palette: ['#f8f9fa', '#fa8d3e', '#f2ae49', '#5c6166', '#399ee6', '#e7eaed'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#f8f9fa' },
+    { name: 'Title', hexCode: '#fa8d3e' },
+    { name: 'Visited Title', hexCode: '#f2ae49' },
+    { name: 'Snippet', hexCode: '#5c6166' },
+    { name: 'URL', hexCode: '#399ee6' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#e7eaed' },
+  ],
   type: 'light',
 };
 
