ed011ee7d1430f7b05874df295c873882af97131
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:34:57 -0600

    fix(themes): update catppuccin latte palette

--- src/data/themes/catppuccinLatte.ts
+++ src/data/themes/catppuccinLatte.ts
@@ -4,7 +4,14 @@ const catppuccinLatte: Theme = {
   name: 'Catppuccin Latte',
   cookie:
     '7=eff1f5; j=eff1f5; 9=1e66f5; aa=7287fd; 8=4c4f69; x=dc8a78; 21=e6e9ef;',
-  palette: ['#eff1f5', '#1e66f5', '#7287fd', '#4c4f69', '#dc8a78', '#e6e9ef'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#eff1f5' },
+    { name: 'Title', hexCode: '#1e66f5' },
+    { name: 'Visited Title', hexCode: '#7287fd' },
+    { name: 'Snippet', hexCode: '#4c4f69' },
+    { name: 'URL', hexCode: '#dc8a78' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#e6e9ef' },
+  ],
   type: 'light',
 };
 
