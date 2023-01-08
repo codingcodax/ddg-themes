a49b9f9cd399bb33dbbfebcbcbcef6278eb7e6f0
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:38:35 -0600

    fix(themes): update retro palette

--- src/data/themes/retro.ts
+++ src/data/themes/retro.ts
@@ -5,13 +5,13 @@ const retro: Theme = {
   cookie:
     '7=fff; j=c65839; 9=000000; aa=11005e; 8=525252; x=9e9ea2; 21=e6e6e6;',
   palette: [
-    '#ffffff',
-    '#c65839',
-    '#000000',
-    '#11005e',
-    '#525252',
-    '#9e9ea2',
-    '#e6e6e6',
+    { name: 'Background', hexCode: '#ffffff' },
+    { name: 'Header', hexCode: '#c65839' },
+    { name: 'Title', hexCode: '#000000' },
+    { name: 'Visited Title', hexCode: '#11005e' },
+    { name: 'Snippet', hexCode: '#525252' },
+    { name: 'URL', hexCode: '#9e9ea2' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#e6e6e6' },
   ],
   type: 'dark',
 };
