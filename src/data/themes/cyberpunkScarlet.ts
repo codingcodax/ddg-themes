de243641c74b9870f77df05eed1f22bdc0b94888
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:36:07 -0600

    fix(themes): update cyberpunk scalet palette

--- src/data/themes/cyberpunkScarlet.ts
+++ src/data/themes/cyberpunkScarlet.ts
@@ -4,7 +4,14 @@ const cyberpunkScarlet: Theme = {
   name: 'Cyberpunk Scarlet',
   cookie:
     '7=101116; j=0a0b0e; 9=ff0055; aa=9d063a; 8=eeffff; x=00ffc8; 21=0a0b0e;',
-  palette: ['#101116', '#0a0b0e', '#ff0055', '#9d063a', '#eeffff', '#00ffc8'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#101116' },
+    { name: 'Title', hexCode: '#ff0055' },
+    { name: 'Visited Title', hexCode: '#9d063a' },
+    { name: 'Snippet', hexCode: '#eeffff' },
+    { name: 'URL', hexCode: '#00ffc8' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#0a0b0e' },
+  ],
   type: 'dark',
 };
 
