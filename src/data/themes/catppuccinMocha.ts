c5b5e2b4366e3c08d7803206a8621fc8b62115ce
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:35:36 -0600

    fix(themes): update catppuccin mocha palette

--- src/data/themes/catppuccinMocha.ts
+++ src/data/themes/catppuccinMocha.ts
@@ -4,7 +4,14 @@ const catppuccinMocha: Theme = {
   name: 'Catppuccin Mocha',
   cookie:
     '7=1e1e2e; j=1e1e2e; 9=89b4fa; aa=b4befe; 8=cdd6f4; x=f5e0dc; 21=11111b;',
-  palette: ['#1e1e2e', '#89b4fa', '#b4befe', '#cdd6f4', '#f5e0dc', '#11111b'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#1e1e2e' },
+    { name: 'Title', hexCode: '#89b4fa' },
+    { name: 'Visited Title', hexCode: '#b4befe' },
+    { name: 'Snippet', hexCode: '#cdd6f4' },
+    { name: 'URL', hexCode: '#f5e0dc' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#11111b' },
+  ],
   type: 'dark',
 };
 
