bb426119422da626562ed061ce753d54d8d02089
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:37:46 -0600

    fix(themes): update neon palette

--- src/data/themes/neon.ts
+++ src/data/themes/neon.ts
@@ -5,7 +5,14 @@ const neon: Theme = {
   cookie:
     // '7=101116; j=070709; 9=00ff2b; aa=0cbd2b; 8=d1d1d1; x=fffc58; 21=118b25; ',
     '7=261d49; j=2a1f48; 9=df95ff; aa=9254b5; 8=1bccfd; x=21f6bc 21=2a1f48;',
-  palette: ['#261d49', '#2a1f48', '#df95ff', '#9254b5', '#1bccfd', '#21f6bc'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#261d49' },
+    { name: 'Title', hexCode: '#2a1f48' },
+    { name: 'Visited Title', hexCode: '#df95ff' },
+    { name: 'Snippet', hexCode: '#9254b5' },
+    { name: 'URL', hexCode: '#1bccfd' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#21f6bc' },
+  ],
   type: 'dark',
 };
 
