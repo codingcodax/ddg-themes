0c0cc9ce580cec8f242198b5f4aae7cfbe702ec7
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:36:58 -0600

    fix(themes): update gruvbox dark palette

--- src/data/themes/gruvbox-dark.ts
+++ src/data/themes/gruvbox-dark.ts
@@ -4,7 +4,14 @@ const gruvboxDark: Theme = {
   name: 'Gruvbox Dark',
   cookie:
     '7=282828; j=282828; 9=fe8019; aa=f9bc2e; 8=fbf1c7; x=b7ba25; 21=3b3735;',
-  palette: ['#282828', '#fe8019', '#f9bc2e', '#fbf1c7', '#b7ba25', '#3b3735'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#282828' },
+    { name: 'Title', hexCode: '#fe8019' },
+    { name: 'Visited Title', hexCode: '#f9bc2e' },
+    { name: 'Snippet', hexCode: '#fbf1c7' },
+    { name: 'URL', hexCode: '#b7ba25' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#3b3735' },
+  ],
   type: 'dark',
 };
 
