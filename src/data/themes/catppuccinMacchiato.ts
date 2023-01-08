271d17a949ce376f79a81399aa42329f8fa41b9b
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:35:24 -0600

    fix(themes): update catppuccin macchiato palette

--- src/data/themes/catppuccinMacchiato.ts
+++ src/data/themes/catppuccinMacchiato.ts
@@ -4,7 +4,14 @@ const catppuccinMacchiato: Theme = {
   name: 'Catppuccin Macchiato',
   cookie:
     '7=24273a; j=24273a; 9=8aadf4; aa=b7bdf8; 8=cad3f5; x=f4dbd6; 21=181926;',
-  palette: ['#24273a', '#8aadf4', '#b7bdf8', '#cad3f5', '#f4dbd6', '#181926'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#24273a' },
+    { name: 'Title', hexCode: '#8aadf4' },
+    { name: 'Visited Title', hexCode: '#b7bdf8' },
+    { name: 'Snippet', hexCode: '#cad3f5' },
+    { name: 'URL', hexCode: '#f4dbd6' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#181926' },
+  ],
   type: 'dark',
 };
 
