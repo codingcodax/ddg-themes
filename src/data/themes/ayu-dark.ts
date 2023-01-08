18c0c75a7a8ef2efc055a68356c29b9512d18649
Alexis Guzman <codingcodax@gmail.com>
2023-01-07 19:31:36 -0600

    fix(themes): update ayu dark palette

--- src/data/themes/ayu-dark.ts
+++ src/data/themes/ayu-dark.ts
@@ -4,7 +4,14 @@ const ayuDark: Theme = {
   name: 'Ayu Dark',
   cookie:
     '7=0b0e14; j=0b0e14; 9=ff8f40; aa=ffb454; 8=bfbdb6; x=59c2ff; 21=11151c;',
-  palette: ['#0b0e14', '#ff8f40', '#ffb454', '#bfbdb6', '#59c2ff', '#11151c'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#0b0e14' },
+    { name: 'Title', hexCode: '#ff8f40' },
+    { name: 'Visited Title', hexCode: '#ffb454' },
+    { name: 'Snippet', hexCode: '#bfbdb6' },
+    { name: 'URL', hexCode: '#59c2ff' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#11151c' },
+  ],
   type: 'dark',
 };
 
