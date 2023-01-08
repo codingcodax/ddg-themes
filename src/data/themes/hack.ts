6c376a414a966ff4b7be1a37d7169645fba2fa87
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:37:22 -0600

    fix(themes): update hack palette

--- src/data/themes/hack.ts
+++ src/data/themes/hack.ts
@@ -4,7 +4,14 @@ const hack: Theme = {
   name: 'Hack',
   cookie:
     '7=070709; j=070709; 9=00ff2b; aa=118b25; 8=d1d1d1; x=fffc58; 21=101116;',
-  palette: ['#070709', '#00ff2b', '#118b25', '#d1d1d1', '#fffc58', '#101116'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#070709' },
+    { name: 'Title', hexCode: '#00ff2b' },
+    { name: 'Visited Title', hexCode: '#118b25' },
+    { name: 'Snippet', hexCode: '#d1d1d1' },
+    { name: 'URL', hexCode: '#fffc58' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#101116' },
+  ],
   type: 'dark',
 };
 
