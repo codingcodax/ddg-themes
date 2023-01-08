d7f3dd55e0deab325c343d495505d74d711b15b7
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:37:38 -0600

    fix(themes): update navy turquoise palette

--- src/data/themes/navyTurquoise.ts
+++ src/data/themes/navyTurquoise.ts
@@ -4,7 +4,14 @@ const navyTurquoise: Theme = {
   name: 'Navy Turquoise',
   cookie:
     '7=0b1021; j=0b1021; 9=00af87; aa=0a7355; 8=d3d5e5; x=a8d3ff; 21=080813;',
-  palette: ['#0b1021', '#00af87', '#0a7355', '#d3d5e5', '#a8d3ff', '#080813'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#0b1021' },
+    { name: 'Title', hexCode: '#00af87' },
+    { name: 'Visited Title', hexCode: '#0a7355' },
+    { name: 'Snippet', hexCode: '#d3d5e5' },
+    { name: 'URL', hexCode: '#a8d3ff' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#080813' },
+  ],
   type: 'dark',
 };
 
