c46036c814548cbf0947a4f18eff22bd428b834d
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:36:20 -0600

    fix(thems): update cyberpunk umbra palette

--- src/data/themes/cyberpunkUmbra.ts
+++ src/data/themes/cyberpunkUmbra.ts
@@ -4,7 +4,14 @@ const cyberpunkUmbra: Theme = {
   name: 'Cyberpunk Umbra',
   cookie:
     '7=100d23; j=1e1d45; 9=0084ff; aa=3d5afe; 8=eeffff; x=00ff9c; 21=1e1d45;',
-  palette: ['#100d23', '#1e1d45', '#0084ff', '#3d5afe', '#eeffff', '#00ff9c'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#100d23' },
+    { name: 'Title', hexCode: '#0084ff' },
+    { name: 'Visited Title', hexCode: '#3d5afe' },
+    { name: 'Snippet', hexCode: '#eeffff' },
+    { name: 'URL', hexCode: '#00ff9c' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#1e1d45' },
+  ],
   type: 'dark',
 };
 
