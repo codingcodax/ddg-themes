bb06a02daea2c9accbe5c44ef406985b73f30cda
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:35:58 -0600

    fix(themes): update cyberpunk palette

--- src/data/themes/cyberpunk.ts
+++ src/data/themes/cyberpunk.ts
@@ -4,7 +4,14 @@ const cyberpunk: Theme = {
   name: 'Cyberpunk',
   cookie:
     '7=261d45; j=372963; 9=0084ff; aa=3d5afe; 8=eeffff; x=00ff9c; 21=372963;',
-  palette: ['#261d45', '#372963', '#0084ff', '#3d5afe', '#eeffff', '#00ff9c'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#261d45' },
+    { name: 'Title', hexCode: '#0084ff' },
+    { name: 'Visited Title', hexCode: '#3d5afe' },
+    { name: 'Snippet', hexCode: '#eeffff' },
+    { name: 'URL', hexCode: '#00ff9c' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#372963' },
+  ],
   type: 'dark',
 };
 
