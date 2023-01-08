7c827df297ed645c3d2e86d5baa02e685bdda3d4
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:34:40 -0600

    fix(themes): update catppuccin frappe palette

--- src/data/themes/catppuccinFrappe.ts
+++ src/data/themes/catppuccinFrappe.ts
@@ -4,7 +4,14 @@ const catppuccinFrappe: Theme = {
   name: 'Catppuccin Frappe',
   cookie:
     '7=303446; j=303446; 9=8caaee; aa=babbf1; 8=c6d0f5; x=eebebe; 21=232634;',
-  palette: ['#303446', '#8caaee', '#babbf1', '#c6d0f5', '#eebebe', '#232634'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#303446' },
+    { name: 'Title', hexCode: '#8caaee' },
+    { name: 'Visited Title', hexCode: '#babbf1' },
+    { name: 'Snippet', hexCode: '#c6d0f5' },
+    { name: 'URL', hexCode: '#eebebe' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#232634' },
+  ],
   type: 'dark',
 };
 
