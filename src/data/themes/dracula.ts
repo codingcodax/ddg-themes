1848a246704a6fccabe2a0eea4cc1f5a38743757
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:36:46 -0600

    fix(themes): update dracula palette

--- src/data/themes/dracula.ts
+++ src/data/themes/dracula.ts
@@ -4,7 +4,14 @@ const dracula: Theme = {
   name: 'Dracula',
   cookie:
     '7=282a36; j=282a36; 9=50fa7b; aa=bd93f9; 8=f8f8f2; x=f1fa8c; 21=44475a;',
-  palette: ['#282a36', '#50fa7b', '#bd93f9', '#f8f8f2', '#f1fa8c', '#44475a'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#282a36' },
+    { name: 'Title', hexCode: '#50fa7b' },
+    { name: 'Visited Title', hexCode: '#bd93f9' },
+    { name: 'Snippet', hexCode: '#f8f8f2' },
+    { name: 'URL', hexCode: '#f1fa8c' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#44475a' },
+  ],
   type: 'dark',
 };
 
