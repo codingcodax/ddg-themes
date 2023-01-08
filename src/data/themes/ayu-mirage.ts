701406c135daf334219faf590faa433991dcf08b
Alexis Guzman <codingcodax@gmail.com>
2023-01-07 19:32:29 -0600

    fix(themes): update ayu mirage palette

--- src/data/themes/ayu-mirage.ts
+++ src/data/themes/ayu-mirage.ts
@@ -4,7 +4,14 @@ const ayuMirage: Theme = {
   name: 'Ayu Mirage',
   cookie:
     '7=1f2430; j=1f2430; 9=ffad66; aa=ffd173; 8=cccac2; x=73d0ff; 21=171b24;',
-  palette: ['#1f2430', '#ffad66', '#ffd173', '#cccac2', '#73d0ff', '#171b24'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#1f2430' },
+    { name: 'Title', hexCode: '#ffad66' },
+    { name: 'Visited Title', hexCode: '#ffd173' },
+    { name: 'Snippet', hexCode: '#cccac2' },
+    { name: 'URL', hexCode: '#73d0ff' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#171b24' },
+  ],
   type: 'dark',
 };
 
