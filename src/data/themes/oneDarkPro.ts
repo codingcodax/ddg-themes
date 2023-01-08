558f95b2220773e99368d766d09309292bd2824d
Alexis Guzman <codingcodax@gmail.com>
2023-01-08 10:38:22 -0600

    fix(themes): update one dark pro palette

--- src/data/themes/oneDarkPro.ts
+++ src/data/themes/oneDarkPro.ts
@@ -4,7 +4,14 @@ const oneDarkPro: Theme = {
   name: 'One Dark Pro',
   cookie:
     '7=282c34; j=282c34; 9=61afef; aa=c678dd; 8=abb2bf; x=e5c07b; 21=21252b;',
-  palette: ['#282c34', '#61afef', '#c678dd', '#abb2bf', '#e5c07b', '#21252b'],
+  palette: [
+    { name: 'Background and Header', hexCode: '#282c34' },
+    { name: 'Title', hexCode: '#61afef' },
+    { name: 'Visited Title', hexCode: '#c678dd' },
+    { name: 'Snippet', hexCode: '#abb2bf' },
+    { name: 'URL', hexCode: '#e5c07b' },
+    { name: 'Hover, Module and Dropdown', hexCode: '#21252b' },
+  ],
   type: 'dark',
 };
 
