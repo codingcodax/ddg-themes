acd821c402ed3539e1a1ccc7abcd95da0d12f9a0
Alexis Guzman <codingcodax@gmail.com>
2023-01-09 16:46:50 -0600

    feat(components): export how to use

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Icons } from './icons';
 export { default as Hero } from './Hero';
 export { default as Seo } from './Seo';
 export { default as Themes } from './themes';
+export { default as HowToUse } from './how-to-use';
