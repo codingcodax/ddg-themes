b841c7739d71003aaf44d0bb0bbfa39ac23b4040
Alexis Guzman <codingcodax@gmail.com>
2023-01-10 11:05:11 -0600

    feat(data): create steps

---
+++ src/data/Steps.ts
@@ -0,0 +1,33 @@
+const Steps = [
+  {
+    number: 1,
+    title: 'Copy',
+    description: 'Select your favorite theme and click on get theme button',
+    image: 'selected-options',
+    alt: 'woman selecting in a number of options',
+    height: 543,
+    width: 660,
+  },
+  {
+    number: 2,
+    title: 'Paste',
+    description:
+      'Go to DuckDuckGo settings page, open the console, paste the code and hit enter.',
+    image: 'color-schemes',
+    alt: 'male selecting palette',
+    height: 516,
+    width: 776,
+  },
+  {
+    number: 3,
+    title: "And that's it!",
+    description:
+      'Your theme was applied and you can also change the colors as you like.',
+    image: 'online-party',
+    alt: 'person celebrating with globes inside of browser',
+    height: 591,
+    width: 745,
+  },
+];
+
+export default Steps;
