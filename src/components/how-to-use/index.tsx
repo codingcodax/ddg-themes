1c7b8cf60b746f5a3ff78473ad7e9a6ad267ce8e
Alexis Guzman <codingcodax@gmail.com>
2023-01-10 11:05:58 -0600

    feat(how): setup how to use

--- src/components/how-to-use/index.tsx
+++ src/components/how-to-use/index.tsx
@@ -1,8 +1,23 @@
+import { Steps } from '~/data';
+
+import Step from './Step';
+
 const HowToUse = () => {
   return (
-    <div>
-      <p>How To Use component</p>
-    </div>
+    <section
+      className='mx-auto mt-16 w-full max-w-screen-lg px-4 md:px-8 lg:px-16'
+      id='howToUse'
+    >
+      <h2 className='mb-4 scroll-m-8 text-center md:mb-6 md:text-left'>
+        How To Use
+      </h2>
+
+      <ul className='grid gap-8 px-4 sm:grid-cols-2 min-[760px]:grid-cols-3 md:px-0'>
+        {Steps.map((step) => (
+          <Step key={step.number} {...step} />
+        ))}
+      </ul>
+    </section>
   );
 };
 
